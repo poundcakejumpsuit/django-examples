19e87a993e3aec8aab5d7e3497595f7f8b6ec4a4
poundcakejumpsuit <woxman01@gmail.com>
2018-06-20 13:57:42 -0400

    added stuff to models

--- tadu/tadu_server/models.py
+++ tadu/tadu_server/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 class User(AbstractUser):
-    pass
+    join_date = models.DateTimeField('join date', default=timezone.now)
+    
+
+class Task(models.Model):
+    task_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    task_text = models.CharField(max_length=300)
+    entry_date = models.DateTimeField('entry date')
+    due_date = models.DateTimeField('due date')
+
+    def __str__(self):
+        return self.task_text
